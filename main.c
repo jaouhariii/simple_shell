90bc8674b7eb3132ffe672bf269cc627ae357393
jaouhariii <nikkovic2@gmail.com>
2023-11-16 04:04:43 -0800

    fix code

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include "shell.h"
-/**
- * main - The Simple Shell main entry point func
- * @ac: Count of argumnents
- * @argv: Arguments
- * Return: 0 Always (success).
-*/
-int main(int ac, char **argv)
-{
-	char *line = NULL;
-	char **command = NULL;
-	int st = 0, idx = 0;
-	(void) ac;
 
-	while (true)
-	line = examin_line();
-	if (line == NULL)
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "\n", 1);
-		return (st);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	idx++;
-	command = theTokenizer(line);
-	if (!command)
-	continue;
-	if (its_builtin(command[0]))
-		builtin_handle(command, argv, &st, idx);
-	else
-		st = _exec(command, argv, idx);
-	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
