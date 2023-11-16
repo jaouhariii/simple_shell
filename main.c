9063a4b369e76cf2b3e974f37710562947ea9d63
Mmed <ed_alh@icloud.com>
2023-11-15 19:07:13 -0800

    updated style

--- main.c
+++ main.c
@@ -9,25 +9,24 @@ int main(int ac, char **argv)
 {
 	char *line = NULL;
 	char **command = NULL;
- 	int st = 0, idx = 0;
- 	(void) ac;
+	int st = 0, idx = 0;
+	(void) ac;
 
 	while (true)
-        line = examin_line();
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            return (st);
-        }
-        idx++;
-        command = theTokenizer(line);
-        if (!command)
-        continue;
-        if (its_builtin(command[0]))
-            builtin_handle(command, argv, &st, idx);
-
-        else
-            st = _exec(command, argv, idx);
-    }
+	line = examin_line();
+	if (line == NULL)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "\n", 1);
+		return (st);
+	}
+	idx++;
+	command = theTokenizer(line);
+	if (!command)
+	continue;
+	if (its_builtin(command[0]))
+		builtin_handle(command, argv, &st, idx);
+	else
+		st = _exec(command, argv, idx);
+	}
 }
