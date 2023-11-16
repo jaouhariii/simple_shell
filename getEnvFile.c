6b2c4bb9f667ce5faca8204a295b3e2635527f36
Mmed <ed_alh@icloud.com>
2023-11-15 20:40:42 -0800

    stylized

--- getEnvFile.c
+++ getEnvFile.c
@@ -6,21 +6,21 @@
 */
 char *_getEn(char *var)
 {
-char *tmp, *key, *value, *env;
-int a;
+	char *tmp, *key, *value, *env;
+	int a;
 
-for (a = 0; environ[a]; a++)
-{
-tmp = _strdup(environ[a]);
-key = strtok(tmp, "=");
-if (_strcmp(key, var) == 0)
-{
-value = strtok(NULL, "\n");
-env = _strdup(value);
-free (tmp);
-return (env);
-}
-free(tmp), tmp = NULL;
-}
-return (NULL);
+	for (a = 0; environ[a]; a++)
+	{
+	tmp = _strdup(environ[a]);
+	key = strtok(tmp, "=");
+	if (_strcmp(key, var) == 0)
+	{
+		value = strtok(NULL, "\n");
+		env = _strdup(value);
+		free(tmp);
+		return (env);
+	}
+	free(tmp), tmp = NULL;
+	}
+	return (NULL);
 }
