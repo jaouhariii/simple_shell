3864c8ec37cf61f97f9ff99b9323659aa83630ed
Mmed <ed_alh@icloud.com>
2023-11-15 19:42:33 -0800

    examineLine.c stylized

--- examineLine.c
+++ examineLine.c
@@ -1,17 +1,21 @@
 #include "shell.h"
+/**
+ * *examin_line - examine and read the line
+ * Return: 0 Always (success).
+*/
 char *examin_line(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t n;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
 
-    if (isatty(STDIN_FILENO) == 1)
-        write(STDOUT_FILENO, "$ ", 2);
-    n = getline(&line, &len, stdin);
-    if (n == -1)
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+	if (isatty(STDIN_FILENO) == 1)
+		write(STDOUT_FILENO, "$ ", 2);
+	n = getline(&line, &len, stdin);
+	if (n == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
