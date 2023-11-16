48a738e10791c78639e9e9da4a54ca0aa5a45030
Edalh <133518813+Edalh@users.noreply.github.com>
2023-11-15 21:53:06 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -19,20 +19,26 @@ char *examin_line(void);
 char **theTokenizer(char *line);
 int _exec(char **command, char **argv, int idx);
 
+char *_getEn(char *var);
+char *_getPath(char *cmd);
 
+/* builtinz */
+int its_builtin(char *command);
+void builtin_handle(char **command, char **argv,int *st, int idx);
+void exit_shell(char **command, char **argv, int *st, int idx);
+void env_print(char **command, int *st);
 
-/* string.C */
-int _strlen (char *s);
+/* strings */
+int _strlen(char *s);
 char *_strdup(const char *str);
-int _strcmp (char *s1, char *s2);
-char _strcat(char *dest, char *src);
-char *_strcpy (char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
-/* tools.C */
+/* tools */
 void freeArrayToD(char **array);
-void print_err(char *name, char *cmd, int idx); 
+void print_error(char *name, char *cmd, int idx); 
 char *_itoa(int n);
-void string_rev(char *str, int len);
+void reverse_string(char *str, int len);
 
 #endif
-
