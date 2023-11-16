22465af3b00270c88970cf31b160b8590bc20f44
Edalh <133518813+Edalh@users.noreply.github.com>
2023-11-16 09:42:00 -0500

    Update handlePath.c

--- handlePath.c
+++ handlePath.c
@@ -22,7 +22,7 @@ char *_getPath(char *cmd)
 	path_env = _getEn("PATH");
 	if (!path_env)
 		return (NULL);
-	dir = strok(path_env, ":");
+	dir = strtok(path_env, ":");
 	while (dir)
 	{
 		full_cmd = malloc(_strlen(dir) + _strlen(cmd) + 2);
@@ -37,7 +37,7 @@ char *_getPath(char *cmd)
 				return (full_cmd);
 			}
 			free(full_cmd), full_cmd = NULL;
-			dir = strok(NULL, ":");
+			dir = strtok(NULL, ":");
 		}
 	}
 	free(path_env);
